29dcb93d1de075bca89a243526eb5acd50ab51c9
Anatoly M <m@m.com>
2020-06-16 02:22:50 +0300

    Create "PosterManager" branch

--- src/main/java/ru/netology/manager/PosterManager.java
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -14,16 +14,9 @@ public class PosterManager {
     }
 
     public void add(MovieUnit item) {
-        // создаём новый массив размером на единицу больше
         int length = items.length + 1;
         MovieUnit[] tmp = new MovieUnit[length];
-        // itar + tab
-        // копируем поэлементно
-        // for (int i = 0; i < items.length; i++) {
-        //   tmp[i] = items[i];
-        // }
         System.arraycopy(items, 0, tmp, 0, items.length);
-        // кладём последним наш элемент
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
@@ -31,8 +24,6 @@ public class PosterManager {
 
     public MovieUnit[] getAll() {
         MovieUnit[] result = new MovieUnit[items.length];
-        // перебираем массив в прямом порядке
-        // но кладём в результаты в обратном
         for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
@@ -47,30 +38,28 @@ public class PosterManager {
             for (int i = 0; i < certainAmount; i++) {
                 int index = items.length - i - 1;
                 result[i] = items[index];
-                System.out.println(items[index]);
             }
         } else {
             for (int i = 0; i < result.length; i++) {
                 int index = items.length - i - 1;
                 result[i] = items[index];
-                System.out.println(items[index]);
             }
         }
         return result;
     }
 
-    // наивная реализация
     public void removeById(int id) {
+        if (id <= items.length) {
         int length = items.length - 1;
         MovieUnit[] tmp = new MovieUnit[length];
         int index = 0;
-        for (MovieUnit item : items) {
-            if (item.getId() != id) {
-                tmp[index] = item;
-                index++;
+            for (MovieUnit item : items) {
+                if (item.getId() != id) {
+                    tmp[index] = item;
+                    index++;
+                }
             }
+            items = tmp;
         }
-        // меняем наши элементы
-        items = tmp;
     }
 }
